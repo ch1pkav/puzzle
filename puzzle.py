c444dca35d1fb84f5b8debebe38eda3e2830092b
ch1pkav <teodor.muzychuk@ucu.edu.ua>
2021-02-09 19:03:08 +0200

    added non-functional puzzle.py

---
+++ puzzle.py
@@ -0,0 +1,23 @@
+"""
+Module for validating puzzle game board.
+
+Github:
+https://github.com/ch1pkav/puzzle
+"""
+def check_for_duplicates():
+    pass
+
+def check_numbers():
+    pass
+
+def check_rows():
+    pass
+
+def check_columns():
+    pass
+
+def check_fields():
+    pass
+
+def validate_board():
+    pass
